d5d0226b204bec7e84835d15d66c233619fe9185
eloylp <eloy@sandboxwebs.com>
2016-07-21 22:23:31 +0200

    Added more tests to request

--- client/requestsadapter.py
+++ client/requestsadapter.py
@@ -1,13 +1,11 @@
-import urllib3
-
 class RequestsAdapter(object):
     api_url = "https://dds.sandboxwebs.com"
     from_header = "DDS-node-id"
 
-    def __init__(self, node_id, auth_token):
+    def __init__(self, node_id, auth_token, request_manager):
         self._node_id = node_id
         self._auth_token = auth_token
-        self.pool = urllib3.PoolManager()
+        self.pool = request_manager
 
     def get_headers(self):
 
@@ -18,15 +16,20 @@ class RequestsAdapter(object):
 
     def request(self, method, resource='', fields=None, headers=None):
 
-        if isinstance(object, headers):
+        if isinstance(headers, object):
             headers.update(self.get_headers())
         else:
             headers = self.get_headers()
-        url = self.api_url + '/' + resource
-
-        res = self.pool.request(method=method, url=url, fields=fields, headers=headers)
+        url = self.get_url(resource)
+        res = self.pool.request(method=method, url=url,
+                                fields=fields, headers=headers)
 
         return res
+
+    def get_url(self, resource):
+
+        url = '/'.join([self.api_url, resource.strip("/")])
+        return url
 
     @property
     def node_id(self):
@@ -35,4 +38,3 @@ class RequestsAdapter(object):
     @property
     def auth_token(self):
         return self._auth_token
-
--- test/requestadapter_test.py
+++ test/requestadapter_test.py
@@ -1,11 +1,26 @@
 import unittest
+from unittest.mock import MagicMock
+import urllib3
 
 from client.requestsadapter import RequestsAdapter
 
 
 class RequestsAdapterTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.request_manager = MagicMock()
+        cls.request_manager.request = cls.request
+
     def setUp(self):
-        self.request_adapter = RequestsAdapter('af123', 'tok')
+        self.request_adapter = RequestsAdapter('af123', 'tok', self.request_manager)
+
+    def request(method, url, fields, headers):
+        return {
+            "method": method,
+            "url": url,
+            "fields": fields,
+            "headers": headers
+        }
 
     def test_fixed_properties(self):
         self.assertEquals('https://dds.sandboxwebs.com', self.request_adapter.api_url)
@@ -14,3 +29,34 @@ class RequestsAdapterTest(unittest.TestCase):
     def test_mandatory_properties(self):
         self.assertEquals('af123', self.request_adapter.node_id)
         self.assertEquals('tok', self.request_adapter.auth_token)
+
+    def test_get_url(self):
+        root = 'https://dds.sandboxwebs.com'
+        self.assertEquals(root + '/resource', self.request_adapter.get_url('/resource'))
+        self.assertEquals(root + '/resource/subresource', self.request_adapter.get_url('/resource/subresource/'))
+
+    def test_get_headers(self):
+        headers = self.request_adapter.get_headers()
+        self.assertEquals(headers['Authorization'], 'tok')
+        self.assertEquals(headers['DDS-node-id'], 'af123')
+
+    def test_request(self):
+
+        fields = {"name": "eloy", "test": True}
+        headers = {"headerExtra": "extraextra!"}
+
+        res = self.request_adapter.request('GET', '/resource',
+                                           fields, headers)
+
+        self.assertEquals(3, len(res['headers']))
+        self.assertEquals('tok', res['headers']['Authorization'])
+        self.assertEquals('extraextra!', res['headers']['headerExtra'])
+        self.assertEquals('af123', res['headers']['DDS-node-id'])
+        self.assertEquals(res['method'], 'GET')
+        self.assertEquals(len(res['fields']), 2)
+        self.assertEquals('eloy', res['fields']['name'])
+        self.assertTrue(res['fields']['test'])
+
+        # TODO. MORE COMBINATIONS .
+
+
