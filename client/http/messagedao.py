da754cc4016554f53b2f5f6bf5b97adb59c3a454
eloylp <eloy@sandboxwebs.com>
2016-08-19 14:56:46 +0200

    Added message dao logic

--- client/http/messagedao.py
+++ client/http/messagedao.py
@@ -21,13 +21,41 @@ class MessageDAO(Base):
             raise SystemError
 
     def get_all(self):
-        return self.request_adapter.request('GET', self.end_point)
+        request_result = self.request_adapter.request('GET', self.end_point)
+
+        if request_result.http_status is 200:
+            ro = ClientMessageResponse()
+            ro.system_data = request_result.system_data
+            ro.message_data = request_result.message_data
+
+            return ro
+        else:
+            raise SystemError
 
     def delete_one(self, id):
-        return self.request_adapter.request('DELETE', ''.join([self.end_point, '/', id]))
+        request_result = self.request_adapter.request('DELETE', ''.join([self.end_point, '/', id]))
+        if request_result.http_status is 200:
+            ro = ClientMessageResponse()
+            ro.system_data = request_result.system_data
+            ro.message_data = request_result.message_data
+            return ro
+        else:
+            raise SystemError
 
     def delete_all(self):
-        return self.request_adapter.request('DELETE', self.end_point)
+        request_result = self.request_adapter.request('DELETE', self.end_point)
+        if request_result.http_status is 200:
+            ro = ClientMessageResponse()
+            ro.system_data = request_result.system_data
+            ro.message_data = request_result.message_data
+            return ro
 
     def update_one(self, id, message):
-        return self.request_adapter.request('PATCH', ''.join([self.end_point, '/', id]), message)
+        request_result = self.request_adapter.request('PATCH', ''.join([self.end_point, '/', id]), message)
+        if request_result.http_status is 200:
+            ro = ClientMessageResponse()
+            ro.system_data = request_result.system_data
+            ro.message_data = request_result.message_data
+            return ro
+        else:
+            raise SystemError
