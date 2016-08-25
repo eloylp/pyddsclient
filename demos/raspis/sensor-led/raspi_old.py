043a1b2018f9951e3003b9015627b7da4eb19192
eloylp <eloy@sandboxwebs.com>
2016-08-25 12:46:40 +0200

    Enabled dds in raspiold example

--- demos/raspis/sensor-led/raspi_old.py
+++ demos/raspis/sensor-led/raspi_old.py
@@ -2,7 +2,7 @@
 
 import RPi.GPIO as GPIO
 import time
-#from sciroccoclient.httpclient import HTTPClient TODO ERROR IMPORT IN RASPI
+from sciroccoclient.httpclient import HTTPClient
 
 
 class RaspiOLdProgram:
@@ -12,22 +12,14 @@ class RaspiOLdProgram:
 
     def run(self):
         us = UltrasonicSensor(23, 24)
-
-        # dds = HTTPClient('https://dds.sandboxwebs.com', 'af12343', 'dd52bb39d5a1bd8f6235dbef7df26d3e')
+        dds = HTTPClient('https://dds.sandboxwebs.com', 'af12343', 'dd52bb39d5a1bd8f6235dbef7df26d3e')
 
         while self.running:
-            print("distance is ...")
-            print(us.make_measurement())
-
-            """msg = dds.message_queue_pull()
-
-            if msg is not None:
-                if msg.message_data['action'] == 'blink':
-                    pass
-                    time.sleep(5)
-                else:
-                    pass
-            """
+            msg_template = {"to_node_id": "", "data": {"measurement": us.make_measurement()}}
+            msg_template['to_node_id'] = 'af12344'
+            dds.message_queue_push(msg_template)
+            msg_template['to_node_id'] = 'af12345'
+            dds.message_queue_push(msg_template)
             time.sleep(self.sleep)
 
 
@@ -57,7 +49,7 @@ class UltrasonicSensor:
         res = self.check_pulse_return_time()
         self.clean()
         result_in_cm = self.do_math_cm(res[0], res[1])
-        
+
         return result_in_cm
 
     def send_pulse(self):
