4e9e0ed13a4b12d9cffc15d483dcc9328a195a02
eloylp <eloy@sandboxwebs.com>
2016-08-25 12:59:46 +0200

    Added interrup exception to raspi_old example

--- demos/raspis/sensor-led/raspi_old.py
+++ demos/raspis/sensor-led/raspi_old.py
@@ -15,12 +15,17 @@ class RaspiOLdProgram:
         dds = HTTPClient('https://dds.sandboxwebs.com', 'af12343', 'dd52bb39d5a1bd8f6235dbef7df26d3e')
 
         while self.running:
-            msg_template = {"to_node_id": "", "data": {"measurement": us.make_measurement()}}
-            msg_template['to_node_id'] = 'af12344'
-            dds.message_queue_push(msg_template)
-            msg_template['to_node_id'] = 'af12345'
-            dds.message_queue_push(msg_template)
-            time.sleep(self.sleep)
+
+            try:
+                msg_template = {"to_node_id": "", "data": {"measurement": us.make_measurement()}}
+                msg_template['to_node_id'] = 'af12344'
+                dds.message_queue_push(msg_template)
+                msg_template['to_node_id'] = 'af12345'
+                dds.message_queue_push(msg_template)
+                time.sleep(self.sleep)
+            except KeyboardInterrupt:
+                self.running = False
+                print("Shutting down measurement system")
 
 
 class UltrasonicSensor:
@@ -44,13 +49,18 @@ class UltrasonicSensor:
 
     def make_measurement(self):
 
-        self.setup_sensor()
-        self.send_pulse()
-        res = self.check_pulse_return_time()
-        self.clean()
-        result_in_cm = self.do_math_cm(res[0], res[1])
+        try:
 
-        return result_in_cm
+            self.setup_sensor()
+            self.send_pulse()
+            res = self.check_pulse_return_time()
+            self.clean()
+            result_in_cm = self.do_math_cm(res[0], res[1])
+
+            return result_in_cm
+        except KeyboardInterrupt:
+            self.clean()
+            raise KeyboardInterrupt
 
     def send_pulse(self):
 
