1f63227ed29830d374630c2c30beab2988e538ad
eloylp <eloy@sandboxwebs.com>
2016-09-07 21:02:14 +0200

    Deleted redundant code for response

--- test/responses_test.py
+++ test/responses_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from sciroccoclient.responses import ClientMessageResponse, ClientBatchResponse
+from sciroccoclient.responses import ClientMessageResponse
 
 
 class ClientMessageResponseTest(unittest.TestCase):
@@ -18,20 +18,3 @@ class ClientMessageResponseTest(unittest.TestCase):
 
     def test_property_system_data_default_none(self):
         self.assertIsNone(self.client_message_response.system_data)
-
-
-class ClientBatchResponseTest(unittest.TestCase):
-    def setUp(self):
-        self.client_batch_response = ClientBatchResponse()
-
-    def test_property_messages_data_exist(self):
-        self.assertTrue(hasattr(self.client_batch_response, 'messages_data'))
-
-    def test_property_messages_data_default_none(self):
-        self.assertIsNone(self.client_batch_response.messages_data)
-
-    def test_property_system_data_exist(self):
-        self.assertTrue(hasattr(self.client_batch_response, 'system_data'))
-
-    def test_property_system_data_default_none(self):
-        self.assertIsNone(self.client_batch_response.system_data)
